37d4d3ba8b3444956e7bcab73325f7691d39cc0f
Boss44521 <54522332+Boss44521@users.noreply.github.com>
2020-11-23 17:56:01 +0700

    update html

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function add() {
+    let x = document.getElementById("x").value;
+    let y = document.getElementById("y").value;
+    let z = x+y
+   
+    
+    document.getElementById('sad').innerHTML = z; 
+}
